b0a4a16fede9d34a957f85d6b4ad6d9b0475e88a
Pruessner, Philip (FDB3) <philip.pruessner@porsche.de>
2026-01-10 01:19:27 +0100

    update tutorial 1

--- Tutorials/1_SmokeTest/tutorial1.py
+++ Tutorials/1_SmokeTest/tutorial1.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 
 def homepage_smoke_test():
@@ -12,7 +13,6 @@ def homepage_smoke_test():
     - Seite lädt ohne Fehler
     - Titel enthält 'Reutlingen'
     - H1-Überschrift enthält 'Hochschule' oder 'Reutlingen'
-    - Navigationslink 'Studium' ist sichtbar
     """
     # Arrange: Browser starten
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
